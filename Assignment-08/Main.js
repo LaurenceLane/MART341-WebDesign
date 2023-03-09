8c5cc22232c6d34be429d5e989de13c46f680073
Laurence Guinard <112394275+LaurenceLane@users.noreply.github.com>
2023-03-08 18:44:45 -0800

    Add files via upload

---
+++ Assignment-08/Main.js
@@ -0,0 +1,33 @@
+function generateHeader() {
+  var headerContent = '<header>\
+    <a href="Index.html"></a>\
+    <img src="Images/LogoColor.png" alt="Logo in Color" width="250px" height="auto" class="logo">\
+    <div class="dropdown">\
+      <button class="dropbtn">Menu</button>\
+      <div class="dropdown-content">\
+        <a href="Index.html">Home</a>\
+        <a href="About.html">About</a>\
+        <a href="Commissions.html">Commissions</a>\
+        <a href="Portfolio.html">Portfolio</a>\
+      </div>\
+    </div>\
+    <br><br><br>\
+   <bar><hr style="height: 5px" color="#243f5c"></bar>\
+   <br><br></br>\
+  </header>';
+  return headerContent;
+}
+
+function disableSubmit() {
+  document.getElementById("Submit").disabled = true;
+  }
+
+
+window.onload = function() {
+  var headerDiv = document.getElementById("header");
+  headerDiv.innerHTML = generateHeader();
+  disableSubmit();
+};
+
+
+
---
+++ Assignment-08/ReadMe.md
@@ -0,0 +1,12 @@
+What is an affordance?
+
+    Afforadances are the parts of a button, or other item, that instructs the user on how it is used and what it does.
+
+What are the advantages and disadvantages of using a third-party service like YouTube or Vimeo to host your videos?
+
+    With 3rd parties, you don't have to hold such large files on your site itself as hosting sites usually have a much smaller limit than the average video. The downside of that is that your video can be taken down by the hosting site, breaking the video on your site.
+
+
+<a href="Images/Screenshot-08.png">Screenshot</a>
+
+![Screenshot](./Images/Screenshot-08.png)
